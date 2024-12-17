4abae5c4e098c76e8c23159da568ea7897694df6
Xander Azuaje <charon-mortis@pm.me>
2024-12-17 09:13:45 +0100

    created logging tool

---
+++ colors/log.go
@@ -0,0 +1,60 @@
+package colors
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+	"time"
+)
+
+type color string
+
+const (
+	Reset   color = "\033[0m"
+	Bold    color = "\033[1m"
+	NoBold  color = "\033[22m"
+	Red     color = "\033[31m"
+	Green   color = "\033[32m"
+	Yellow  color = "\033[33m"
+	Blue    color = "\033[34m"
+	Magenta color = "\033[35m"
+	Cyan    color = "\033[36m"
+	White   color = "\033[37m"
+)
+
+var colorMap = map[rune]color{
+	'*': Bold,
+	'r': Red,
+	'g': Green,
+	'y': Yellow,
+	'b': Blue,
+	'm': Magenta,
+	'c': Cyan,
+	'w': White,
+}
+
+func Log(format string, data ...any) {
+	r := regexp.MustCompile("@[rgybmcw*]{1,2}\\(.[^)]*\\)")
+	normal := r.Split(format, -1)
+	values := r.FindAllString(format, -1)
+	var str string
+	for i, v := range normal {
+		str += v
+		if i < len(values) {
+			v = values[i][1:]
+			for _, c := range v {
+				if c == '(' {
+					break
+				}
+				str += string(colorMap[c])
+			}
+			v = strings.TrimLeft(v, "*rgybmcw")
+			v = strings.Trim(v, "()")
+			str += v
+			str += string(Reset)
+		}
+	}
+	print(time.Now().Format("06/01/02 15:04:05"), "   ")
+	fmt.Printf(str, data...)
+	print("\n")
+}
